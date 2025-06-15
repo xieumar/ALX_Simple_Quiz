3ea5f0f471d9d1be49c4cfb8d1a7a54d22827606
Zainab Umar Ibrahim <xieumar@gmail.com>
2025-06-15 20:40:39 +0100

    made minor changes

--- quiz.js
+++ quiz.js
@@ -3,7 +3,7 @@ function checkAnswer() {
     var userAnswer = document.querySelector('input[name="quiz"]:checked').value;
     
 
-    if(userAnswer === correctAnswer) {
+   if (userAnswer === correctAnswer) {
         document.getElementById("feedback").textContent = "Correct! Well done.";
     }
     else{
